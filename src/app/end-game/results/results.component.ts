df458e9e90523d01c16c30fd6f97fa845be67d67
ianrose <ianrose@umich.edu>
2024-03-21 12:30:49 -0500

    re-styled results, added high score

--- src/app/end-game/results/results.component.ts
+++ src/app/end-game/results/results.component.ts
@@ -14,18 +14,37 @@ import { LeaderboardEntry, StorageService } from 'src/services/storage.service';
 export class ResultsComponent implements OnInit {
   configForm!: FormGroup;
   score: number = 0
+  alert: boolean = false;
+  alertMessage: string = "";
 
   constructor(private fb: FormBuilder, private storageService: StorageService, private router: Router) { }
 
   ngOnInit(): void {
-    this.createForm()
+    this.createForm();
     this.score = this.storageService.load('playerScore');
+    this.highScore(this.score);
   }
 
   private createForm() {
     this.configForm = this.fb.group({
       name: ["", Validators.required]
     });
+  }
+
+  // alerts user if they got a new high score for their current genre
+  private highScore(playerScore: number) {
+    const genre = this.storageService.load("gameSettings").genreName;
+    const scoresByGenre = this.storageService.getLeaderboard(genre);
+    if (scoresByGenre.length === 0) {
+      this.alert = true;
+      this.alertMessage = "New High Score!"
+      return
+    }
+    const highestScore = scoresByGenre.reduce((max, current) => max > current.score ? max : current.score, scoresByGenre[0].score);
+    if (playerScore > highestScore) {
+      this.alert = true;
+      this.alertMessage = "New High Score!"
+    }
   }
 
   onSubmit() {
@@ -42,11 +61,17 @@ export class ResultsComponent implements OnInit {
         this.configForm.reset();
         this.router.navigateByUrl("/leaderboard")
       } else {
-        alert("Game settings or player score missing.");
+        this.alert = true;
+        this.alertMessage = "Game settings or player score missing.";
       }
     } else {
-      alert("Please enter your name.");
+      this.alert = true;
+      this.alertMessage = "Please enter your name.";
     }
+  }
+
+  closeAlert() {
+    this.alert = false;
   }
 
 
